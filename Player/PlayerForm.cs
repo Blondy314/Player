ca880c215fd42861b5c713d4551a3ad08adbe758
Inbar Rotem <inbar.rotem@gmail.com>
2020-08-27 21:52:01 +0300

    Minor resharper fixes

--- Player/PlayerForm.cs
+++ Player/PlayerForm.cs
@@ -9,11 +9,9 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using BrightIdeasSoftware;
 using PcapDotNet.Core;
-using PcapDotNet.Core.Extensions;
 using PcapDotNet.Packets;
 using PcapDotNet.Packets.Ethernet;
 using PcapDotNet.Packets.IpV4;
-using PcapDotNet.Packets.Transport;
 
 namespace Player
 {
